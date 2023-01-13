38d0e2d29619587398984c5d9af2231128e3f4ec
Abiraman K <abiramancit@gmail.com>
2023-01-13 19:40:21 +0530

    [Abiraman K] Removes unwanted drawer main list items

--- src/utilities/listItems.tsx
+++ src/utilities/listItems.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import { Dashboard as DashboardIcon, Calculate } from "@mui/icons-material";
+import { Dashboard as DashboardIcon } from "@mui/icons-material";
 
 export interface OperandListInterface {
   value: string;
@@ -16,12 +16,6 @@ export const mainListItems = (
         <DashboardIcon />
       </ListItemIcon>
       <ListItemText primary="Dashboard" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <Calculate />
-      </ListItemIcon>
-      <ListItemText primary="Calculations" />
     </ListItemButton>
   </React.Fragment>
 );
